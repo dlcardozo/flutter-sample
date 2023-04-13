14e3140bbead30438aecd5dafb1be763ba1e4414
dcardozo <diiego.cardozo@gmail.com>
2023-04-13 17:57:10 -0300

    add scaffold to register_page

--- lib/users/presentation/register_page.dart
+++ lib/users/presentation/register_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sample/users/presentation/register_page_view_model.dart';
 
 class RegisterPage extends StatefulWidget {
   const RegisterPage({Key? key}) : super(key: key);
@@ -8,8 +10,32 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  late RegisterPageViewModel _viewModel;
+
+  @override
+  void initState() {
+    _viewModel = RegisterPageViewModel();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Register'),
+      ),
+      body: Wrap(
+        children: [
+          ChangeNotifierProvider.value(
+            value: _viewModel,
+            child: Consumer<RegisterPageViewModel>(
+              builder: (BuildContext context, viewModel, Widget? child) {
+                return const Placeholder();
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
